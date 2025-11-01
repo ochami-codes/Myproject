c42a0ab8896ecfa882063114e7dab20b456debc8
ochami-codes <ochamiian29@gmail.com>
2025-11-01 23:23:47 +0300

    Initial commit

---
+++ My-project/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './assets/Header';
+import Hero from './assets/Hero';
+import About from './assets/About';
+import project from './assets/project';
+import contacts from './assets/contacts';
+import './App.css';
+export default function App() {
+  return (
+      <>
+            <Header />
+                  <Hero />
+                        <About />
+                              <project />
+                                    <contacts />
+                                        </>
+                                          );
+                                          }
---
+++ README.md
@@ -0,0 +1,3 @@
+# Myproject
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ochami-codes/Myproject)
